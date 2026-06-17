6b7fa04b33127c6ce6d45eaaa1c0769504fc730c
maxim-okolot <maximokolot@gmail.com>
2026-06-18 01:45:34 +0300

    Fix reply-message blinking and align chat scripts with production.
    Assign data-id when rendering log rows, replace legacy showdiv with blinking(msgId), add blink/reply-ref styles, and fix selectroom markup plus sidebar clock in chat.inc.

--- assets/js/script.js
+++ assets/js/script.js
@@ -962,114 +962,6 @@ var SNAMIK_BOT = (function () {
 })(); // --- END SNAMIK BOT v5 --------------------------------------
 
 //  --- НАСТРОЙКИ ЧАТА -----------------------------------------------
-const settings = {
-  mediaSrc: '../assets', // относительный адрес к медиа файлам
-  defaultFont: { // Стандартные настройки шрифтов для ника и текста сообщений
-    weight: 'black',
-    family: 'Comic Sans MS'
-  },
-
-  defaultIcon: `${this.mediaSrc}/img/users/default-icon.jpg`, // Стандартная иконка
-
-  nomerp: 0,
-  nomers: 0,
-  repeat: 0, // Количество сообщений в топике. 0 - отмена
-  topicMessages: 15, // Через сколько сообщений
-  numberRoom: 0, // Номер комнаты в которой выводить топик
-  chatImages: 0, // 1 - картинки не отображаются, 0 - отображаются
-  roomLog: 0, // Загрузка последних сообщений при переходе в комнату, 1 - вкл (перезагрузка фрейма)
-  privateFrame: 0, // Приват в отдельном фрейме: 0 - выкл, 1 - вкл (необходимо настроить фреймовую систему)
-  genderSeparate: 0, // Использовать никлист с разделением по полам: 0 - выкл, 1 - вкл
-  moderStats: [1, 2, 3, 8, 14, 11, 12], // Статусы модераторов
-  topic: '<div class="topic"></div>',  // Топик - выводится во фрейме сообщений сразу после загрузки
-
-  // Стартовая разметка истории сообщений
-  myhistory1: '<!DOCTYPE html><html lang="ru"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><link rel="stylesheet" type="text/css" href="style.css"></head><body class="header-body">',
-  myhistory: '',
-
-  setcursor: 1, // Автофокус поля ввода при наборе текста, 1 - вкл
-  nomousemenu: 0, // Запретить правую кнопку мышки, 1 - вкл
-  slowscroll: 12, // Скорость прокрутки фрейма сообщений, 0 - выкл, 15 - плавно
-  maxmsgs: 0, // Максимальное кол-во сообщений во фрейме, 0 - неограниченно
-  maxabc: 5, // Ограничение повторяющихся символов в сообщениях до value, 0 - выкл
-
-  graphNick: 0, // Использовать графники 0 - выкл, 1 - вкл. Добавления графников (img/flash) в gn.js
-  gradientNick: 0, // градиентные ники 0 - выкл, 1 - вкл
-  gradientText: 0, // градиентный текст gr.js
-}
-
-//  --- ПРИВИЛЕГИИ ЧАТА -----------------------------------------------
-const privileges = {
-  // Невидимки:
-  invisible: [],
-
-  // Очистить фрейм (/clear):
-  clearer: ['Домовенок'],
-
-  // Удаление сообщений командой (/remove):
-  remover: ['FunnyBunny'],
-
-  // Доступ к объявлению (/obya):
-  ad_access: ['FunnyBunny'],
-
-  // Перезагрузить чат (/reload):
-  reloader: ['FunnyBunny'],
-
-  // Вызвать модальное окно (/alert):
-  alerter: [],
-
-  // Перенаправление в комнаты чата
-  censor: ['FunnyBunny'],
-
-  // Установить тотальный игнор (/ignore):
-  ignorer: ['FunnyBunny'],
-}
-
-//  --- ЛИЧНЫЕ ФРАЗЫ -----------------------------------------------
-const phrases = {
-  /* Приветствие при входе в чат, используйте %nick% для вставки ника. 0 - дефолтный вариант */
-  greeting: ['К нам приходит %nick%. Всем привет!', [['Фортуна'], 'Меня зовут, %nick%  и мне поручено передать вам привет. От кого? От моего сердца вам мои любимые!']],
-
-  /* Прощание при выходе из чата. 0 - дефолтный вариант */
-  farewell: ['От нас уходит %nick%. Всем пока!', ['FunnyBunny', 'SweetBanny'], 'От нас уходит Администратор чата %nick%. Всем пока, до скорого!']
-}
-
-// --- СТАТУСЫ ПОЛЬЗОВАТЕЛЕЙ ------------------------------------------
-const status = {
-  0: `<img title="Гость" src="${settings.mediaSrc}/img/status/0.gif">`,
-  1: `<img title="Админ" src="${settings.mediaSrc}/img/status/1.gif">`,
-  2: `<img title="Заместитель админа" src="${settings.mediaSrc}/img/status/2.gif">`,
-  3: `<img title="Главный модератор" src="${settings.mediaSrc}/img/status/0.gif">`,
-  4: `<img title="Пользователь" src="${settings.mediaSrc}/img/status/0.gif">`,
-  5: `<img title="Житель" src="${settings.mediaSrc}/img/status/0.gif">`,
-  6: `<img title="Техник" src="${settings.mediaSrc}/img/status/6.png">`,
-  7: `<img title="Радиоведущий" src="${settings.mediaSrc}/img/status/7.png">`,
-  8: `<img title="Радиоведушая" src="${settings.mediaSrc}/img/status/8.png">`,
-  9: `<img title="Модератор" src="${settings.mediaSrc}/img/status/9.png">`,
-}
-
-const currentStatus = {
-  0: `<img title="свободен" src="${settings.mediaSrc}/img/status/current/0.gif">`,
-  1: `<img title="работаю" src="${settings.mediaSrc}/img/status/current/1.gif">`,
-  2: `<img title="влюблен" src="${settings.mediaSrc}/img/status/current/2.gif">`,
-  3: `<img title="меня нет" src="${settings.mediaSrc}/img/status/current/3.gif">`,
-  4: `<img title="сплю" src="${settings.mediaSrc}/img/status/current/4.gif">`,
-  5: `<img title="кушаю" src="${settings.mediaSrc}/img/status/current/5.gif">`,
-  6: `<img title="бухаю" src="${settings.mediaSrc}/img/status/current/6.gif">`,
-  7: `<img title="курю" src="${settings.mediaSrc}/img/status/current/7.gif">`,
-  8: `<img title="принимаю ванну" src="${settings.mediaSrc}/img/status/current/8.gif">`,
-  9: `<img title="слушаю музыку" src="${settings.mediaSrc}/img/status/current/9.gif">`,
-  10: `<img title="в эфире" src="${settings.mediaSrc}/img/status/current/10.gif">`,
-  11: `<img title="в ярости" src="${settings.mediaSrc}/img/status/current/11.gif">`,
-}
-
-/* --- СТАТУСЫ СООБЩЕНИЙ -----------------------------------------------
-0 — Простое в общий чат
-1 — Кому-то в общий чат
-2 — Мне в общий чат
-3 — Приватно кому-то
-4 — Приватно мне
-*/
 
 
 /* Топик - выводится в фрейме сообщений сразу после загрузки */
@@ -1240,20 +1132,18 @@ stn2[12] = '<img src=https://chat.radio-paradise.de/chat/img/2834168126.gif?0 wi
 
 /* Сообщение о смене статуса */
 var icqtxt = new Array();
-var imgChangeStatus = `${settings.mediaSrc}/img/status/current/change-status.png`;
-
-icqtxt[0] = `<img src="${imgChangeStatus}">свободен <img src="${settings.mediaSrc}/img/status/current/0.gif">`;
-icqtxt[1] = `<img src="${imgChangeStatus}">работаю <img src="${settings.mediaSrc}/img/status/current/1.gif">`;
-icqtxt[2] = `<img src="${imgChangeStatus}">влюблен{на} <img src="${settings.mediaSrc}/img/status/current/2.gif">`;
-icqtxt[4] = `<img src="${imgChangeStatus}">меня нет <img src="${settings.mediaSrc}/img/status/current/3.gif">`;
-icqtxt[5] = `<img src="${imgChangeStatus}">сплю <img src="${settings.mediaSrc}/img/status/current/4.gif">`;
-icqtxt[6] = `<img src="${imgChangeStatus}">кушаю <img src="${settings.mediaSrc}/img/status/current/5.gif">`;
-icqtxt[7] = `<img src="${imgChangeStatus}">бухаю <img src="${settings.mediaSrc}/img/status/current/6.gif">`;
-icqtxt[8] = `<img src="${imgChangeStatus}">курю <img src="${settings.mediaSrc}/img/status/current/7.gif">`;
-icqtxt[9] = `<img src="${imgChangeStatus}">водн.процедуры <img src="${settings.mediaSrc}/img/status/current/8.gif">`;
-icqtxt[10] = `<img src="${imgChangeStatus}">слушаю музыку <img src="${settings.mediaSrc}/img/status/current/9.gif">`;
-icqtxt[11] = `<img src="${imgChangeStatus}">в эфире <img src="${settings.mediaSrc}/img/status/current/10.gif">`;
-icqtxt[12] = `<img src="${imgChangeStatus}">в ярости <img src="${settings.mediaSrc}/img/status/current/11.gif">`;
+icqtxt[0] = '<img src="https://i.postimg.cc/LXnYHPzX/3.png">свободен <img src="https://i.postimg.cc/Px2gMLcm/im-free-cute.gif">';
+icqtxt[1] = '<img src="https://i.postimg.cc/LXnYHPzX/3.png">работаю <img src="https://i.postimg.cc/RhJHTtF8/ryry-slept-on-me-milk-and-mocha.gif">';
+icqtxt[2] = '<img src="https://i.postimg.cc/LXnYHPzX/3.png">влюблен{на} <img src="https://i.postimg.cc/6Q18FrMM/hugging-heart-snoopy.gif">';
+icqtxt[4] = '<img src="https://i.postimg.cc/LXnYHPzX/3.png">меня нет <img src="https://i.postimg.cc/kggtjLBK/the-cat-line.gif">';
+icqtxt[5] = '<img src="https://i.postimg.cc/LXnYHPzX/3.png">сплю <img src="https://i.postimg.cc/HWSmwKDf/the-cat-line-1.gif">';
+icqtxt[6] = '<img src="https://i.postimg.cc/LXnYHPzX/3.png">кушаю <img src="https://i.postimg.cc/BQSwZQjF/birbhaus-food.gif">';
+icqtxt[7] = '<img src="https://i.postimg.cc/LXnYHPzX/3.png">бухаю <img src="https://i.postimg.cc/R0XJMQqr/lethargic-bliss-wine.gif">';
+icqtxt[8] = '<img src="https://i.postimg.cc/LXnYHPzX/3.png">курю <img src="https://i.postimg.cc/66mvF04F/smoke-cash-2.gif">';
+icqtxt[9] = '<img src="https://i.postimg.cc/LXnYHPzX/3.png">водн.процедуры <img src="https://i.postimg.cc/kG95K22N/relax-relaxing.gif">';
+icqtxt[10] = '<img src="https://i.postimg.cc/LXnYHPzX/3.png">слушаю музыку <img src="https://i.postimg.cc/cHFzMjHD/listening-musica.gif">';
+icqtxt[11] = '<img src="https://i.postimg.cc/LXnYHPzX/3.png">в эфире <img src="https://i.postimg.cc/B65Kr9Dg/live-on-air.gif">';
+icqtxt[12] = '<img src="https://i.postimg.cc/LXnYHPzX/3.png">в ярости <img src="https://i.postimg.cc/158z61tj/dudu-cooking-bubu-fire.gif">';
 
 
 /* Авто смена статуса при молчании */
@@ -1312,39 +1202,20 @@ const sound = {
 }
 
 
+/* Мерцание отвеченного сообщения */
+function blinking(msgId) {
+  if (msgId == null || msgId === '') return false;
+  let id = String(msgId);
+  let div = document.querySelector(`#leftdiv > div[data-id="${id}"]`);
+  if (!div) return false;
 
-/* Мигалка */
-var showdivtimer = new Array();
+  div.classList.add('blink');
 
-function showdiv(id, cmd, repet, opacity, n) {
-  repet = repet || 11;
-  //сколько раз мигать +1
-  opacity = opacity || 100;
-  n = n || -10;
-  if (opacity == 100) {
-    n = -10;
-    repet--
-  }
-  if (opacity == 0) {
-    n = 10;
-  }
-  var div = document.getElementById(id);
-  if (cmd == 1 && privatok == 1) {
-    var privatDiv = document.getElementById(id);
-    if (privatDiv) div = privatDiv;
-  }
-  if (!div) return;
-  if (window.ActiveXObject) {
-    var h = div.offsetHeight;
-    div.style.height = h;
-    div.style.filter = "progid:DXImageTransform.Microsoft.Alpha(opacity=" + opacity + ")";
-  } else {
-    div.style.opacity = opacity / 100
-  }
-  ;opacity += n;
-  showdivtimer[id] = setTimeout("showdiv('" + id + "'," + cmd + "," + repet + "," + opacity + "," + n + ")", 100);
-  if (repet == 0)
-    clearTimeout(showdivtimer[id]);
+  setTimeout(function() {
+    div.classList.remove('blink');
+  }, 3000);
+
+  return true;
 }
 
 /* Функция изменения загружаемой картинки */
@@ -1590,6 +1461,13 @@ function wr(text) {
   }
   var div = document.createElement('div');
   div.innerHTML = text;
+  var timeEl = div.querySelector('.time-message');
+  var timeStr = timeEl ? timeEl.textContent : null;
+  if (!timeStr) {
+    var timeMatch = text.match(/\b(\d{2}:\d{2}:\d{2})\b/);
+    if (timeMatch) timeStr = timeMatch[1];
+  }
+  if (timeStr) div.dataset.id = timeStr.replace(/\:/g, '');
   leftdiv.appendChild(div);
   if (loaded == 1) up();
 }
@@ -1884,7 +1762,7 @@ function f(room, cmd, nick, tonick, text, time, colornick, color, var9, var10, v
           }
         }
       }
-      ;
+
       if (nick != mynick || timeremovez == null) return;
       text = 'Вы удалили сообщение/я с ' + timeremovez;
     }
@@ -1930,10 +1808,8 @@ function f(room, cmd, nick, tonick, text, time, colornick, color, var9, var10, v
       if (tonick && mynick != tonick && loaded == 1) ign_sel(tonick);
       if (nick != mynick || !tonick) return;
       if (ign_ok(tonick)) text = "Вы поставили полный игнор на ник " + tonick; else text = "Вы сняли полный игнор с ника " + tonick;
-
     }
   }
-
 
   /* Вывод пользователя в другую комнату */
   if (text.indexOf('/deportation') == 0 && censor[nick]) {
@@ -1959,22 +1835,17 @@ function f(room, cmd, nick, tonick, text, time, colornick, color, var9, var10, v
     time = d.toTimeString().substr(0, 8);
   }
 
-  /* Мигалка */
-  var timeremovez = [];
-  var timeremovez = text.match(/см\.\s(\d\d.\d\d.\d\d)/g);
+  /* Добавление мерцания (ссылка «см. ЧЧ:ММ:СС» → data-id на строке лога) */
+   timeremovez = text.match(/см\.\s(\d\d.\d\d.\d\d)/g);
+
   if (timeremovez != null) {
-    for (var i = 0; i < timeremovez.length; i++) {
-      var obj = document.getElementById("leftdiv");
-      var div = obj.getElementsByTagName('div');
-      for (var k = 0; k < div.length; k++) {
-        if (div[k].innerHTML.search(timeremovez[i]) >= 0) {
-          timeremoveid = timeremovez[i].replace(/см\.\s|\:/g, "");
-          text = text.replace(timeremovez[i], "<span  onclick=\"showdiv('" + timeremoveid + "',0);\">" + timeremovez[i] + "</span>");
-          div[k].id = timeremoveid;
-          if (loaded == 1 && mynick == tonick)
-            showdiv(timeremoveid, 0)
-        }
-      }
+    for (let i = 0; i < timeremovez.length; i++) {
+      var msgId = timeremovez[i].replace(/см\.\s|\:/g, '');
+      text = text.replace(
+        timeremovez[i],
+        '<span class="reply-ref" onclick="blinking(\'' + msgId + '\'); return false;">' + timeremovez[i] + '</span>'
+      );
+      if (loaded === 1 && mynick === tonick) blinking(msgId);
     }
   }
 
